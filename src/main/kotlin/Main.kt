841fd8165b56b4bd86683ea037904c5641bc4450
Naketsmall <57672953+Naketsmall@users.noreply.github.com>
2022-08-11 16:10:39 +0300

    Delete Main.kt

--- src/main/kotlin/Main.kt
+++
@@ -1,5 +0,0 @@
-fun main() {
-    val parser = Parser("https://m.avito.ru/moskva_i_mo/kommercheskaya_nedvizhimost/prodam-ASgBAgICAUSwCNJW?cd=1&f=ASgBAgECAkSwCNJW8hKg2gEBRcaaDBd7ImZyb20iOjkwMDAwMDAsInRvIjowfQ&s=104&user=1",
-    "https://discord.com/api/webhooks/996020661065617438/yxD7Vih4u8xcx1g8lnYd5UV6XMZ83KCq0azGvPH6Md9IRWEuYttkEgLZxi-ErpBN9pfW")
-    parser.start()
-}
